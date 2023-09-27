9465796c60ea5a849c29b02fce0c84ffed1a054b
Daniel Akselrod <dakselrod@outlook.com>
2023-09-26 21:48:35 -0400

    connect to Azure SQL Server database

---
+++ src/SweatSmart-Service/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SweatSmart_Service.Database;
+using SweatSmart_Service.Models;
+
+namespace SweatSmart_Service.Controllers;
+
+public class UserController : ControllerBase
+{
+    private readonly SweatSmartDbContext _context;
+
+    public UserController(SweatSmartDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("/add-user")]
+    public IActionResult AddUser()
+    {
+        var user = new User { Id = 1, Name = "John Doe" };
+        _context.Users.Add(user);
+        _context.SaveChanges();
+
+        return Ok();
+    }
+    
+    [HttpGet("/get-user")]
+    public IActionResult GetUsers()
+    {
+        int num = 1;
+        var evenIdUsers = _context.Users.FromSqlRaw("SELECT * FROM Users WHERE Id % " + num + " = 0").ToList();
+        return Ok(evenIdUsers);
+        
+    }
+}
