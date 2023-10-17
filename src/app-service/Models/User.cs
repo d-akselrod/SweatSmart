bb2378cb3a5b1d8c33341775a6a8a5d775c1e275
Daniel Akselrod <dakselrod@outlook.com>
2023-10-17 15:00:42 -0400

    (#88)add user model

--- src/app-service/Models/User.cs
+++ src/app-service/Models/User.cs
@@ -1,7 +1,33 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace App_Service.Models;
 
+[Table("Users")]
 public class User
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
+    [Key]
+    [Column("uID")]
+    public Guid uID { get; set; }
+
+    [Column("FirstName")]
+    [MaxLength(32)]
+    public string firstName { get; set; }
+
+    [Column("LastName")]
+    [MaxLength(32)]
+    public string lastName { get; set; }
+
+    [Column("username")]
+    [MaxLength(32)]
+    public string username { get; set; }
+
+    [Column("password")]
+    [MaxLength(32)]
+    public string password { get; set; }
+
+    [Column("email")]
+    [MaxLength(64)]
+    public string email { get; set; }
 }
