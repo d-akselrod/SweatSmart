a4a0bd2fac7ab66d5ddc7598c786bb332f824f39
Daniel Akselrod <dakselrod@outlook.com>
2023-10-18 14:39:09 -0400

    refactor models

--- src/app-service/Models/PhysicalAttributes.cs
+++ src/app-service/Models/PhysicalAttributes.cs
@@ -1,27 +1,25 @@
-using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace App_Service.Models
+namespace App_Service.Models;
+
+[Table("PhysicalAttributes")]
+public class PhysicalAttributes
 {
-    [Table("PhysicalAttributes")]
-    public class PhysicalAttributes
-    {
-        [Key]
-        [Column("uID")]
-        public Guid uID { get; set; }
+    [Key]
+    [Column("uID")]
+    public Guid uID { get; set; }
 
-        [Column("age")]
-        public int? age { get; set; }
+    [Column("age")]
+    public int? age { get; set; }
 
-        [MaxLength(50)]
-        [Column("gender")]
-        public string gender { get; set; }
+    [MaxLength(32)]
+    [Column("gender")]
+    public string gender { get; set; }
 
-        [Column("height", TypeName = "decimal(5,2)")]
-        public decimal? height { get; set; }
+    [Column("height", TypeName = "decimal(5,2)")]
+    public decimal? height { get; set; }
 
-        [Column("weight", TypeName = "decimal(5,2)")]
-        public decimal? weight { get; set; }
-    }
+    [Column("weight", TypeName = "decimal(5,2)")]
+    public decimal? weight { get; set; }
 }
