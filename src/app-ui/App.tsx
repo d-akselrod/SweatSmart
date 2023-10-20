8005962c59e02895851bd6081ebeb40c1624acf4
Daniel <dakselrod@outlook.com>
2023-10-19 20:47:58 -0400

    make frontend ready for dev

--- src/app-ui/App.tsx
+++ src/app-ui/App.tsx
@@ -1,56 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {request, CRUD, API_URL, API_AUTH} from './service/config';
+import React from 'react';
+import { StyleSheet, View, Image } from 'react-native';
 
 export default function App() {
-
-  const [userName, setUserName] = useState('');
-
-  useEffect(() => {
-    fetch(`http://192.168.1.28:5042/${'User'}`, {
-      'method': 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': API_AUTH
-      },
-    }).then(res => {
-      if (!res.ok) {
-        throw new Error(`HTTP error! Status: ${res.status}`);
-      }
-      return res.json();
-    })
-        .then((data) => console.log(data))
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-          console.error("URL:", `${API_URL}/${'User'}`);
-        });
-  }, []);
-  
-  // useEffect(() => {
-  //   const responseAction = (data: any) => {
-  //     setUserName(data[0].username);
-  //   };
-  //
-  //   const errorAction = (error: any) => {
-  //       console.error("Error fetching username:", error);
-  //   }
-  //
-  //   request('User', CRUD.read, undefined, responseAction, errorAction)
-  // }, []);
-
   return (
-      <View style={styles.container}>
-        <Text>{userName}</Text>
-        <StatusBar style='auto' />
-      </View>
+    <View style={styles.container}>
+      <Image
+        source={require('./assets/Logo.png')}
+        style={{
+          width: 300,
+          height: 300,
+          tintColor: 'black',
+          objectFit: 'scale-down',
+        }}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- src/app-ui/service/config.ts
+++ src/app-ui/service/config.ts
@@ -1,27 +1,7 @@
 // @ts-ignore
-import {API_URL_LOCAL, API_URL_DEPLOY, API_JWT} from '@env';
+import { API_URL_LOCAL, API_URL_DEPLOY, API_JWT } from '@env';
 
 const isLocalDev = false;
 
 export const API_URL = isLocalDev ? API_URL_LOCAL : API_URL_DEPLOY;
 export const API_AUTH = `Bearer ${API_JWT}`;
-
-export enum CRUD{
-    create = "post",
-    read = "get",
-    update = "put",
-    delete = "delete"
-}
-
-export const request = async (endpoint: string, method: CRUD, body?: any, responseAction? : Function, errorAction? : Function) => {
-    return fetch(`http://192.168.1.10:5042/${endpoint}`, {
-        method,
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': API_AUTH
-        },
-        body: JSON.stringify(body)
-    }).then(res => res.json())
-        .then(responseAction ? data => responseAction(data) : () => {})
-        .then(errorAction ? error => errorAction(error) : () => {});
-}
