bc5a25133065d5a081087cddab52b06cb52afed2
Daniel Akselrod <dakselrod@outlook.com>
2023-10-23 14:45:28 -0400

    refactor response codes

--- src/app-service/services/AccountService.cs
+++ src/app-service/services/AccountService.cs
@@ -1,5 +1,3 @@
-using System.Security.Authentication;
-using System.Text.RegularExpressions;
 using App_Service.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using App_Service.Database;
@@ -19,92 +17,66 @@ public record RegisterRequest(string Username, string Email, string Password);
 public class AccountService : ControllerBase
 {
     private readonly DatabaseContext database;
+    private readonly UserController userController;
 
     public AccountService(DatabaseContext database)
     {
         this.database = database;
+        userController = new UserController(database);
     }
 
     [Authorize]
     [HttpPost("Login")]
     public async Task<IActionResult> Login(LoginRequest body)
     {
-        try
+        var username = body.Username;
+        var email = body.Email;
+        var password = body.Password;
+
+        if ((username == null && email == null) || (username != null && email != null))
         {
-            var username = body.Username;
-            var email = body.Email;
-            var password = body.Password;
-
-            if ((username == null && email == null) || (username != null && email != null) || password == null)
-            {
-                return BadRequest();
-            }
-
-            var user = username != null
-                ? await database.Users.SingleOrDefaultAsync(user => user.Username == username)
-                : await database.Users.SingleOrDefaultAsync(user => user.Email == email);
-
-            if (user == null)
-            {
-                return username == null
-                    ? StatusCode(1002)
-                    : StatusCode(1003);
-            }
-
-            return password == user.Password ? Ok() : StatusCode(1004);
+            return APIResponse.BadRequest;
         }
-        catch (Exception)
+        var user = username != null
+            ? await database.Users.SingleOrDefaultAsync(user => user.Username == username)
+            : await database.Users.SingleOrDefaultAsync(user => user.Email == email);
+
+        if (user == null)
         {
-            return StatusCode(500);
+            return APIResponse.NotFound;
         }
+
+        return password == user.Password
+            ? APIResponse.Ok
+            : APIResponse.Unauthorized;
     }
 
     [Authorize]
     [HttpPost("Register")]
     public async Task<IActionResult> Register(RegisterRequest body)
     {
-        try
+        var username = body.Username;
+        var email = body.Email;
+        var password = body.Password;
+
+        if (await database.Users.AnyAsync(user => user.Email == email))
         {
-            var username = body.Username;
-            var email = body.Email;
-            var password = body.Password;
-
-            if (username == null || email == null || password == null)
-            {
-                return BadRequest();
-            }
-
-            if (await database.Users.AnyAsync(user => user.Email == email))
-            {
-                return StatusCode(1000);
-            }
-
-            if (await database.Users.AnyAsync(user => user.Username == username))
-            {
-                return StatusCode(1001);
-            }
-
-            // Regex pattern = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,32}$");
-            // if (pattern.IsMatch(password) == false)
-            // {
-            //     return StatusCode(1005);
-            // }
-
-            UserController userController = new UserController(database);
-
-            var newUser = new User
-            {
-                Email = email,
-                Username = username,
-                Password = password
-            };
-
-            userController.AddUser(newUser);
-            return Ok(newUser);
+            return APIResponse.Conflict;
         }
-        catch (Exception)
+
+        if (await database.Users.AnyAsync(user => user.Username == username))
         {
-            return StatusCode(500);
+            return APIResponse.Conflict;
         }
+
+        var newUser = new User
+        {
+            Email = email,
+            Username = username,
+            Password = password
+        };
+
+        await userController.AddUser(newUser);
+        return APIResponse.Ok;
     }
 }
