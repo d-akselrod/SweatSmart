2270f622f2328f8a8fa6024b6b5857fe9aaaf175
Daniel Akselrod <dakselrod@outlook.com>
2023-10-28 17:59:22 -0400

    (#65)add JWT auth to chatbot endpoint

--- src/app-ui/service/ChatBotAPI.ts
+++ src/app-ui/service/ChatBotAPI.ts
@@ -1,11 +1,12 @@
 import { IChatMessage } from '../pages/chatbot/types';
-import { API_URL } from './config';
+import {API_AUTH, API_URL} from './config';
 
 export const sendPrompt = (history: IChatMessage[]) => {
   return fetch(`${API_URL}/ChatBot`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      'Authorization': API_AUTH
     },
     body: JSON.stringify(history),
   });
