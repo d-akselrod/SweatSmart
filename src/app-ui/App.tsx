bb1a12923ce9900fe8a0ad03b9e18f232895ad11
Daniel Akselrod <dakselrod@outlook.com>
2023-10-29 03:31:52 -0400

    (#62)add app entry and navigation

--- src/app-ui/App.tsx
+++ src/app-ui/App.tsx
@@ -1,18 +1,61 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import RegistrationPage from './pages/register/RegisterPage';
+import { EntryPage } from '../app-ui/pages/entry/EntryPage';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useSelector } from 'react-redux';
+import { Provider } from 'react-redux';
+import { store } from '../app-ui/redux/store';
+import RegisterPage from './pages/register/RegisterPage';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+const EmptyPage = () => <></>;
+
+const AppEntry = () => (
+  <NavigationContainer>
+    <Stack.Navigator
+      initialRouteName='Entry'
+      screenOptions={{
+        headerShown: false,
+        cardStyleInterpolator: ({ current }) => ({
+          cardStyle: {
+            opacity: current.progress,
+          },
+        }),
+      }}
+    >
+      <Stack.Screen name='Entry' component={EntryPage} />
+      <Stack.Screen name='Login' component={EmptyPage} />
+      <Stack.Screen name='Registration' component={RegisterPage} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+const AppMain = () => (
+  <NavigationContainer>
+    <Tab.Navigator
+      initialRouteName='Home'
+      screenOptions={{ headerShown: false }}
+    >
+      <Tab.Screen name='ChatBot' component={EmptyPage} />
+      <Tab.Screen name='Profile' component={EmptyPage} />
+      <Tab.Screen name='Home' component={EmptyPage} />
+      <Tab.Screen name='Planner' component={EmptyPage} />
+      <Tab.Screen name='Progress' component={EmptyPage} />
+    </Tab.Navigator>
+  </NavigationContainer>
+);
+
+const App = () => {
+  const activeUser = useSelector((state: any) => state.user);
+  return activeUser == undefined ? <AppEntry /> : <AppMain />;
+};
+
+export default function Root() {
   return (
-    <View style={styles.container}>
-      <RegistrationPage></RegistrationPage>
-    </View>
+    <Provider store={store}>
+      <App />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'fff',
-  },
-});
---
+++ src/app-ui/pages/entry/EntryPage.tsx
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from 'react';
+import { View, Animated, StyleSheet } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { setActiveUser } from '../../redux/slices/userSlice';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
+
+export const EntryPage = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
+  const [logoScale] = useState(new Animated.Value(2));
+  const [logoRotation] = useState(new Animated.Value(0));
+
+  const fetchUserData = async () => {
+    try {
+      const userJson = await AsyncStorage.getItem('user');
+      return userJson ? JSON.parse(userJson) : null;
+    } catch (error) {
+      console.error('Failed to fetch user data:', error);
+    }
+  };
+
+  const handleAnimationCompletion = async () => {
+    const activeUser = await fetchUserData();
+
+    if (activeUser) {
+      dispatch(setActiveUser(activeUser));
+    } else {
+      // @ts-ignore
+      navigation.navigate('Registration');
+    }
+  };
+
+  useEffect(() => {
+    Animated.sequence([
+      Animated.delay(500),
+      Animated.parallel([
+        Animated.timing(logoRotation, {
+          toValue: 1.5,
+          duration: 1500,
+          useNativeDriver: true,
+        }),
+        Animated.timing(logoScale, {
+          toValue: 3,
+          duration: 1000,
+          useNativeDriver: true,
+        }),
+      ]),
+      Animated.timing(logoScale, {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: true,
+      }),
+    ]).start(handleAnimationCompletion);
+  }, [dispatch]);
+
+  return (
+    <View style={styles.container}>
+      <Animated.Image
+        source={require('../../assets/Logo.png')}
+        style={[
+          styles.logo,
+          {
+            transform: [
+              { scale: logoScale },
+              {
+                rotate: logoRotation.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: ['0deg', '360deg'],
+                }),
+              },
+            ],
+          },
+        ]}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+});
