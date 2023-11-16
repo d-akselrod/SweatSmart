12cbf27d5d294fe339a7485f046e6e153efe1f05
Daniel <dakselrod@outlook.com>
2023-11-15 19:01:17 -0500

    format code

--- src/app-ui/service/AccountAPI.ts
+++ src/app-ui/service/AccountAPI.ts
@@ -40,24 +40,3 @@ export const loginAccount = (usernameOrEmail: string, password: string) => {
   });
 };
 
-export const loginAccount = (
-    usernameOrEmail: string,
-    password: string,
-) => {
-
-  const loginBody = {
-    Username: usernameOrEmail.includes('@') ? null : usernameOrEmail,
-    Email: usernameOrEmail.includes('@') ? usernameOrEmail : null,
-    Password: password
-  }
-
-  return fetch(`${API_URL}/AccountService/Login`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: API_AUTH,
-    },
-    body: JSON.stringify(loginBody),
-  });
-};
-
