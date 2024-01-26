70f14736c5d81d0b34fa5e3bce30689fb323b365
sammcdonald14 <129910152+sammcdonald14@users.noreply.github.com>
2024-01-26 16:35:33 -0500

    new UserPreferences model

---
+++ src/app-service/models/UserPreferences.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace App_Service.Models;
+
+[Table("UserPreferences")]
+public class UserPreferences
+{
+    [Key]
+    [Required]
+    [Column("uID", TypeName = "UniqueIdentifier")]
+    public Guid UId { get; set; }
+
+    [Required]
+    [Column("Goal", TypeName = "VarChar(32)")]
+    public string Goal { get; set; }
+
+    [Required]
+    [Column("ExperienceLevel", TypeName = "Char(1)")]
+    public char ExperienceLevel { get; set; }
+
+    [Required]
+    [Column("Frequency", TypeName = "Int")]
+    public int Frequency { get; set; }
+
+    [Required]
+    [Column("EquipmentAvailable", TypeName = "Char(1)")]
+    public char EquipmentAvailable { get; set; }
+
+    [Required]
+    [Column("TimeAvailable", TypeName = "Int")]
+    public int TimeAvailable { get; set; }
+
+}
