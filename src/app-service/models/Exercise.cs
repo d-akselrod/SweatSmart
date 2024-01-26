0f4646c759be66e2bc6e0f9ed30fd0a54ea19ada
sammcdonald14 <129910152+sammcdonald14@users.noreply.github.com>
2024-01-26 16:38:11 -0500

    Updated exercise to match model in database

--- src/app-service/models/Exercise.cs
+++ src/app-service/models/Exercise.cs
@@ -1,28 +1,39 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace App_Service.Models;
-
-[Table("Exercises")]
-public class Exercise
+namespace App_Service.Models
 {
-    [Required]
-    [Key]
-    [Column("eID", TypeName = "UniqueIdentifier")]
-    public Guid EId { get; set; }
+    [Table("Exercises")]
+    public class Exercise
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("eID")]
+        public Guid EId { get; set; }
 
-    [Required]
-    [Column("Name", TypeName = "VarChar(32)")]
-    public string Name { get; set; }
+        [Required]
+        [Column("MuscleGroup", TypeName = "varchar(32)")]
+        public required string MuscleGroup { get; set; }
 
-    [Required]
-    [Column("Description", TypeName = "Text")]
-    public string Description { get; set; }
+        [Required]
+        [Column("Name", TypeName = "varchar(32)")]
+        public required string Name { get; set; }
 
-    [Required]
-    [Column("MuscleGroup", TypeName = "VarChar(32)")]
-    public string MuscleGroup { get; set; }
+        [Required]
+        [Column("Level", TypeName = "char(1)")]
+        public required string Level { get; set; }
+        
+        [Required]
+        [Column("U_L_C", TypeName = "char(1)")] 
+        public required char? UI_C { get; set; }
 
-    [Column("Duration", TypeName = "Time(0)")]
-    public TimeSpan? Duration { get; set; }
+        [Column("P_P", TypeName = "varchar(4)")] 
+        public char? P_P { get; set; }
+
+        [Required]
+        [Column("Equipment", TypeName = "char(1)")]
+        public required string Equipment { get; set; }
+
+    }
 }
