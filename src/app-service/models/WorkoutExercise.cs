35887c401a4d5b2c7020d346fd3f921622295612
sammcdonald14 <129910152+sammcdonald14@users.noreply.github.com>
2024-01-26 22:45:17 -0500

    Models

---
+++ src/app-service/models/WorkoutExercise.cs
@@ -0,0 +1,35 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace App_Service.Models
+{
+    public class WorkoutExercise
+    {
+
+        [Key]
+        [Required]
+        [Column("weID", TypeName = "UniqueIdentifier")]
+        public Guid WEId { get; set; }
+
+        [Required]
+        [ForeignKey("Exercise")]
+        public Guid EId { get; set; }
+
+        [Required]
+        [ForeignKey("Workout")]
+        public Guid WId { get; set; }
+
+        [Required]
+        [Column("Sets", TypeName = "int")]
+        public int Sets { get; set; }
+
+        [Required]
+        [Column("Reps", TypeName = "int")]
+        public int Reps { get; set; }
+        
+        [Required]
+        [Column("PercentageOfOneRepMax", TypeName = "double")]
+        public double PercentageOfOneRepMax { get; set; }
+    }
+}
