7a1008ae72476876508768a4126aa0ff21137471
Daniel Akselrod <dakselrod@outlook.com>
2024-01-31 03:24:04 -0500

    add base help page. behaviour still needed

--- src/app-ui/pages/profile/subpages/HelpPage.tsx
+++ src/app-ui/pages/profile/subpages/HelpPage.tsx
@@ -1,9 +1,11 @@
+import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { SafeAreaView, Text } from 'react-native';
+import {SafeAreaView, ScrollView, Text, TextInput, StyleSheet, Button} from 'react-native';
 
 export const HelpPage = () => {
   const navigation = useNavigation();
+  const [feedback, setFeedback] = useState('');
 
   useEffect(() => {
     navigation.setOptions({
@@ -13,5 +15,52 @@ export const HelpPage = () => {
     });
   });
 
-  return <SafeAreaView />;
+  const handleFeedbackSubmit = () => {
+    // Implement the logic to handle the feedback submission
+    console.log(feedback); // For example, log it to the console or send it to a server
+  };
+
+  return (
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.scrollView}>
+          <Text style={styles.headerText}>Submit your feedback</Text>
+          <TextInput
+              style={styles.input}
+              placeholder="Your feedback"
+              value={feedback}
+              onChangeText={setFeedback}
+              multiline
+          />
+          <Button
+              title="Submit Feedback"
+              onPress={handleFeedbackSubmit}
+          />
+        </ScrollView>
+      </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+  },
+  scrollView: {
+    marginHorizontal: 20,
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginVertical: 20,
+  },
+  input: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 20,
+    borderRadius: 5,
+    minHeight: 300,
+    backgroundColor: '#fff',
+    textAlignVertical: 'top', // to start text from the top in multiline input
+  }
+});
