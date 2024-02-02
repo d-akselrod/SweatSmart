c796328f1425ca75d7d3ebe12f7ef0a78a3a0fbe
sammcdonald14 <129910152+sammcdonald14@users.noreply.github.com>
2024-02-02 11:53:26 -0500

    created WorkoutPlanAPI, edited WorkoutCard to include sets and reps in the workout card

--- src/app-ui/pages/progress/WorkoutCard.tsx
+++ src/app-ui/pages/progress/WorkoutCard.tsx
@@ -1,24 +1,43 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { IWorkout } from '../../typings/types';
+//import { Picker } from '@react-native-picker/picker';
 
 export interface IWorkoutCardProps extends IWorkout{
-    exercises?: string[];
+    exercises: string[];
+    sets: number[];
+    reps: number[];
 }
 
 export function WorkoutCard(props: IWorkoutCardProps) {
-    const { name, date, duration, numOfExercises, exercises, } = props;
+    const { name, date, duration, exercises, sets, reps } = props;
+    const numOfExercises = exercises.length;
     const formattedDate = date.toLocaleDateString();
     return (
         <View style={styles.container}>
             <Text style={styles.date}>{formattedDate}</Text>
             <Text style={styles.title}>{name}</Text>
-            <Text style={styles.content}>{duration}</Text>
-            <Text style={styles.content}>{numOfExercises}</Text>
+            <Text style={styles.content}>{`${duration} minutes`}</Text>
+            {exercises.map((exercise, index) => (
+                <Text key={index} style={styles.content}>
+                    {`${exercise} - ${sets[index]} sets of ${reps[index]} reps`}
+                </Text>
+            ))}
         </View>
-    )
+    );
 }
-
+            {/* <Picker
+                selectedValue={numOfExercises}
+                style={{ height: 50, width: 150 }}
+            >
+                {exercises.map((exercise, index) => (
+                    <Picker.Item 
+                        key={index} 
+                        label={`${exercise} - ${sets[index]} sets of ${reps[index]} reps`} 
+                        value={exercise} 
+                    />
+                ))}
+            </Picker> */}
 const styles = StyleSheet.create({
     container: {
         backgroundColor: 'white',
